5665e02ec5f9e482e5eda24c5454bdca9584c1bc
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-04-16 15:39:32 +0100

    add home icon and current q to quizpage

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 
-const two = document.querySelector(".t2")
-const container = document.querySelector("#flex-container")
-const subtitle = document.createElement('div')
-const subText = subtitle.innerHTML = '2 Times Table'
-const questionBox = document.createElement('div')
-const question = questionBox.innerHTML = '2x2'
+const two = document.querySelector('.t2');
+
+const container = document.querySelector('#flex-container'); // aka main
+const body = document.querySelector('body');
+
+const subtitle = document.createElement('div'); // create subtitle
+let subText = subtitle.innerHTML = '2 Times Table';
+
+const questionBox = document.createElement('div'); // create question box
+let question = questionBox.innerHTML = '2x2';
+
+const footer = document.createElement('footer'); // create footer
+
+const home = document.createElement('img'); // create home icon
+home.setAttribute('src', 'assets/iconfinder_-_Home-House-_3844470.svg');
+home.setAttribute('alt', 'Home Icon');
+
+const questionNum = document.createElement('p');
+let currentQ = questionNum.innerHTML = '1/12'
+
 
 two.addEventListener('click', function startGameTwo(){
-console.log("selected two times table");
+console.log('selected two times table');
 while (container.firstChild) { // removes all the tiles
     container.removeChild(container.lastChild);
   }
-console.log("removed");
+console.log('removed');
 
-container.appendChild(subtitle)
-console.log("appended subtitle")
+container.classList.add('quizpage'); // add class to main to apply css
 
-subtitle.classList.add('subtitle')
+container.appendChild(subtitle)// append subtitle
+console.log('appended subtitle');
+subtitle.classList.add('subtitle'); // add class to subtitle to apply css
 
-container.appendChild(questionBox)
-console.log("appended question")
+container.appendChild(questionBox);// append question box
+console.log('appended question');
+questionBox.classList.add('questionBox'); // add class to questionbox to apply css
 
-questionBox.classList.add('questionBox')
-container.classList.add('quizpage')
+body.appendChild(footer);// append footer
+footer.appendChild(home); //insert home icon
+home.classList.add('home'); // add class to home to apply css
+footer.appendChild(questionNum) // insert q num
+questionNum.classList.add('questionNum'); // add class to questionNum to apply css
 
 })
