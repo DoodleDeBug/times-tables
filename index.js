b7c253317c29034cee6f4410ce0e32b464f3c9d2
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-04-16 15:17:58 +0100

    make subtitle and question box and add styling

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+
 const two = document.querySelector(".t2")
 const container = document.querySelector("#flex-container")
-const tiles = document.querySelectorAll(".tile")
-
-
+const subtitle = document.createElement('div')
+const subText = subtitle.innerHTML = '2 Times Table'
+const questionBox = document.createElement('div')
+const question = questionBox.innerHTML = '2x2'
 
 two.addEventListener('click', function startGameTwo(){
 console.log("selected two times table");
-while (container.firstChild) {
+while (container.firstChild) { // removes all the tiles
     container.removeChild(container.lastChild);
   }
 console.log("removed");
+
+container.appendChild(subtitle)
+console.log("appended subtitle")
+
+subtitle.classList.add('subtitle')
+
+container.appendChild(questionBox)
+console.log("appended question")
+
+questionBox.classList.add('questionBox')
+container.classList.add('quizpage')
+
 })
