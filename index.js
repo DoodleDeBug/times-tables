de43ff0ba174484cd49801f9917b017b7190bb56
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-04-16 15:51:42 +0100

    home button works

--- index.js
+++ index.js
@@ -16,31 +16,33 @@ const home = document.createElement('img'); // create home icon
 home.setAttribute('src', 'assets/iconfinder_-_Home-House-_3844470.svg');
 home.setAttribute('alt', 'Home Icon');
 
-const questionNum = document.createElement('p');
-let currentQ = questionNum.innerHTML = '1/12'
+const questionNum = document.createElement('p'); // create q num tracker
+let currentQ = questionNum.innerHTML = '1/12'; // set first q to num 1/12
 
 
 two.addEventListener('click', function startGameTwo(){
-console.log('selected two times table');
+
 while (container.firstChild) { // removes all the tiles
     container.removeChild(container.lastChild);
   }
-console.log('removed');
 
 container.classList.add('quizpage'); // add class to main to apply css
 
 container.appendChild(subtitle)// append subtitle
-console.log('appended subtitle');
 subtitle.classList.add('subtitle'); // add class to subtitle to apply css
 
 container.appendChild(questionBox);// append question box
-console.log('appended question');
 questionBox.classList.add('questionBox'); // add class to questionbox to apply css
 
 body.appendChild(footer);// append footer
+
 footer.appendChild(home); //insert home icon
 home.classList.add('home'); // add class to home to apply css
+home.addEventListener('click', goHome)
+function goHome(){
+    location.href = '/index.html';
+ }
+
 footer.appendChild(questionNum) // insert q num
 questionNum.classList.add('questionNum'); // add class to questionNum to apply css
-
 })
