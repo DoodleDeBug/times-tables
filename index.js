4a9cd1ff582ec456e7c4d0311c6c8c0672b2edca
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-05-19 20:32:08 +0100

    clean code

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-// get all the quiz tiles
+// ****************get all the quiz tiles
 const two = document.querySelector(".t2");
 
 //get main body bits
 const container = document.querySelector("#flex-container"); // aka main
 const body = document.querySelector("body");
 
-const subtitle = document.createElement("div"); // create subtitle
-subText = "2 Times Table";
-subtitle.innerHTML = subText;
-
-const questionBox = document.createElement("div"); // create question box
-const question = document.createElement("div"); // create div for q
-const input = document.createElement("input"); // create input box
-input.setAttribute("type", "text");
-input.setAttribute("id", "userInput");
-
-const button = document.createElement("div"); // create next button
-button.innerText = "Press Enter To Continue";
-button.classList.add("button"); // add class to button to apply css
-
 const footer = document.createElement("footer"); // create footer
 
 const home = document.createElement("img"); // create home icon
 home.setAttribute("src", "assets/home.svg");
 home.setAttribute("alt", "Home Icon");
+home.classList.add("home"); // add class to home to apply css
+home.addEventListener("click", goHome);
+
+// game
+two.addEventListener("click", createPage);
+two.addEventListener("click", playRound);
+
+//*************things i cant put in create page coz of scope issues
+const subtitle = document.createElement("div"); // create subtitle
+const question = document.createElement("div"); // create div for q
+const input = document.createElement("input"); // create input box
 
 const questionNum = document.createElement("p"); // create q num tracker
 let qNum = 1;
 
 let numbers = Array.from(Array(12).keys(), (n) => n + 1); // create array 1 to 12
-// console.log(numbers);
 
+let q = "";
+let actualAnswer = "";
+let results = [];
+let scores = 0;
+
+const goodMsg = "Well Done!";
+const badMsg = "You Need To Revise!";
+
+//***************************FUNCTIONS
 function goHome() {
   location.href = "./index.html"; // home button takes you back to index.html w/out js applied yet
 }
@@ -51,22 +56,28 @@ function createPage() {
 
   container.classList.add("quizpage"); // add class to main to apply css
 
+  subText = "2 Times Table";
+  subtitle.innerHTML = subText;
   container.appendChild(subtitle); // append subtitle
   subtitle.classList.add("subtitle", "t2"); // add class to subtitle to apply css
 
+  const questionBox = document.createElement("div"); // create question box
   container.appendChild(questionBox); // append question box
   questionBox.classList.add("questionBox", "t2"); // add class to questionbox to apply css
   questionBox.appendChild(question); // add the q before the input
-  input.classList.add("input");
 
+  input.setAttribute("type", "text");
+  input.setAttribute("id", "userInput");
+  input.classList.add("input");
   questionBox.appendChild(input); // add input onto page
 
+  const button = document.createElement("div"); // create next button
+  button.innerText = "Press Enter To Continue";
+  button.classList.add("button"); // add class to button to apply css
   questionBox.appendChild(button); // add button onto questionBox
-  body.appendChild(footer); // append footer
 
+  body.appendChild(footer); // append footer
   footer.appendChild(home); //insert home icon
-  home.classList.add("home"); // add class to home to apply css
-  home.addEventListener("click", goHome);
 
   footer.appendChild(questionNum); // insert q num
   questionNum.classList.add("questionNum"); // add class to questionNum to apply css
@@ -74,45 +85,36 @@ function createPage() {
   document.addEventListener("keypress", enterNext);
 }
 
-let q = "";
-let actualAnswer = "";
-
 function playRound() {
   // start the game
 
   let type = Array.from(subText)[0];
-  // // console.log(type);
 
   let random = numbers[Math.floor(Math.random() * numbers.length)];
-  // // // console.log(random);
+
   q = `${type} x ${random}`;
   let fullQ = `What is ${q}?`;
-  // // console.log(q);
+
   actualAnswer = type * random;
-  // console.log(actualAnswer);
 
   if (qNum <= 12) {
     question.innerText = fullQ;
     questionNum.innerText = `${qNum}/12`;
     qNum++;
   } else if (qNum > 12) {
-    console.log("game over");
     createResults();
   }
 }
 
-two.addEventListener("click", createPage);
-two.addEventListener("click", playRound);
-
-//gathering results
-
-let results = [];
-let scores = 0;
-
+//gathering results of each answer
 function nextQ() {
   let answer = input.value;
   let fullAns = `${q} = ${answer}`;
-  // console.log(fullAns);
+
+  input.value = "";
+  mark();
+  playRound();
+
   function mark() {
     // mark the answer and store it in a variable with full ans and whether correct or not
     window["q" + qNum] = {
@@ -122,25 +124,15 @@ function nextQ() {
     if (answer == actualAnswer) {
       window["q" + qNum].mark = "correct";
       scores++;
-      // console.log("correct");
     } else {
       window["q" + qNum].mark = "wrong";
-      // console.log("wrong");
     }
-
-    // console.log(window["q" + qNum]);
-
     results.push(window["q" + qNum]);
   }
-
-  mark();
-  input.value = "";
-  playRound();
 }
-// console.log(results);
 
+// results page layout
 function createResults() {
-  // results page layout
   while (container.firstChild) {
     // removes all the tiles
     container.removeChild(container.lastChild);
@@ -152,7 +144,9 @@ function createResults() {
   container.appendChild(subtitle); // append subtitle
   subtitle.classList.add("subtitle", "t2"); // add class to subtitle to apply css
   subtitle.innerHTML = `${subText} - SCORES`;
+
   footer.removeChild(questionNum); // remove q num
+
   const resultsContainer = document.createElement("div");
   resultsContainer.classList.add("results-container");
   container.appendChild(resultsContainer);
@@ -168,15 +162,14 @@ function createResults() {
   const resultsMessage = document.createElement("div");
   resultsMessage.classList.add("results-message");
   container.appendChild(resultsMessage);
+
   const score = document.createElement("p");
   score.innerText = `Your Score: ${scores}/12`;
   resultsMessage.appendChild(score);
+
   const msg = document.createElement("p");
-  msg.innerText = `Well Done`;
+  scores > 5 ? (msg.innerText = goodMsg) : (msg.innerText = badMsg);
   resultsMessage.appendChild(msg);
 
   document.removeEventListener("keypress", enterNext);
 }
-
-const goodMsg = "Well Done!";
-const badMsg = "You Need To Revise!";
